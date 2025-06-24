3ec8bdbdc22b48497e19c26c35a6fb72f0daa352
Adam Blanchard <adamgoodie@gmail.com>
2025-06-24 14:26:44 +0200

    added skeleton exercises for intro to backend

---
+++ intro-to-backend/exercise2.js
@@ -0,0 +1,29 @@
+import express from "express";
+import knex from "knex";
+
+const app = express();
+const port = 3000;
+
+// This connects to the database stored in the file mentioned below
+const knexInstance = knex({
+  client: "sqlite3",
+  connection: {
+    filename: "./database.sqlite3",
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello exercise 2!");
+});
+
+// Here is an example of the first route, /all-users, which returns all users sorted by their ID
+app.get("/all-users", async (req, res) => {
+  const rows = await knexInstance.raw("SELECT * FROM users ORDER BY id ASC;");
+  res.json(rows);
+});
+
+// TODO implement more routes here
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
