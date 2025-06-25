f831e2a2d622240949f8c30fc3ddca0551f056a1
Adam Blanchard <adamgoodie@gmail.com>
2025-06-25 10:00:55 +0200

    Update intro-to-backend/exercise1.js
    Co-authored-by: Marco Richetta <marcorichetta@gmail.com>

--- intro-to-backend/exercise1.js
+++ intro-to-backend/exercise1.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello exercise 1!");
+  res.send("Hello from exercise 1!");
 });
 
 app.get("/currentYear", (req, res) => {
