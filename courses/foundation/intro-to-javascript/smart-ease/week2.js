27a4bcd81d26cc5e27f2a6866653071f0d2bf90a
Tracy Trà Trần <tratran.315@gmail.com>
2025-11-16 23:02:13 +0100

    Create event application

--- courses/foundation/intro-to-javascript/smart-ease/week2.js
+++ courses/foundation/intro-to-javascript/smart-ease/week2.js
@@ -20,3 +20,25 @@ const fullName2 = getFullName("Mette", "Andersen", false, "female");
 console.log(`First customer: ${fullName1}.`, `Second customer: ${fullName2}.`);
 
 console.log(getFullName());
+
+// Event application
+function getEventWeekday(day) {
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const todayIndex = new Date().getDay();
+  // console.log(todayIndex);
+
+  const eventDayIndex = day % 7;
+  const eventDay = weekdays[todayIndex + eventDayIndex];
+
+  return console.log(`The event will be held on ${eventDay}.`);
+}
+
+getEventWeekday(11);
