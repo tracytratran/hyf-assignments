fc56b07375eaee098906b4be3f1175f761cf46a9
Tracy Trà Trần <tratran.315@gmail.com>
2026-04-06 14:12:28 +0200

    Fetch data from API and populate the currency dropdowns with default values

--- courses/frontend/advanced-javascript/week3/script.js
+++ courses/frontend/advanced-javascript/week3/script.js
@@ -1,1 +1,48 @@
 "use strict";
+const currencySelectorFrom = getElement("#currency-from");
+const currencySelectorTo = getElement("#currency-to");
+
+function getElement(el) {
+  return document.querySelector(el);
+}
+
+function createEl(el) {
+  return document.createElement(el);
+}
+
+async function fetchExchangeRates() {
+  try {
+    const response = await fetch("https://open.er-api.com/v6/latest/USD");
+    const data = await response.json();
+
+    renderCurrencySelector(currencySelectorFrom, data.rates);
+    renderCurrencySelector(currencySelectorTo, data.rates);
+  } catch (error) {
+    console.error(error);
+    // Then show error message to user
+  }
+}
+
+await fetchExchangeRates();
+
+function renderCurrencySelector(parentEl, obj) {
+  for (const [key, value] of Object.entries(obj)) {
+    const currencyOption = createEl("option");
+    currencyOption.value = key;
+    currencyOption.textContent = key;
+
+    if (parentEl === currencySelectorFrom) {
+      if (key === "EUR") {
+        currencyOption.selected = "selected";
+      }
+    }
+
+    if (parentEl === currencySelectorTo) {
+      if (key === "DKK") {
+        currencyOption.selected = "selected";
+      }
+    }
+
+    parentEl.appendChild(currencyOption);
+  }
+}
