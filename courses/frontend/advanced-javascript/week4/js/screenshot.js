8363993267a68c5cffb0873cc8068feb0df2ab66
Tracy Trà Trần <tratran.315@gmail.com>
2026-04-17 11:32:11 +0200

    Add screenshot wrapper to delete individual screenshot instead of the whole screenshot container

--- courses/frontend/advanced-javascript/week4/js/screenshot.js
+++ courses/frontend/advanced-javascript/week4/js/screenshot.js
@@ -4,7 +4,7 @@ import ApiService from "./api-service.js";
 import { ValidationError, AppError } from "./error-system.js";
 
 export default class Screenshot {
-  constructor(inputUrl, screenshotUrl, onError) {
+  constructor(inputUrl, screenshotUrl, onError, isSaved = false) {
     if (!inputUrl || !screenshotUrl) {
       throw new ValidationError("Input URL and screenshot URL are required!");
     }
@@ -12,8 +12,10 @@ export default class Screenshot {
     this.inputUrl = inputUrl;
     this.screenshotUrl = screenshotUrl;
     this.onError = onError;
+    this.isSaved = isSaved;
     this.apiService = new ApiService();
     this.screenshotContainer = document.querySelector(".screenshot-container");
+    this.screenshotWrapper = null;
 
     if (!this.screenshotContainer) {
       throw new AppError("Screenshot container element not found!");
@@ -21,23 +23,28 @@ export default class Screenshot {
   }
 
   render() {
+    this.screenshotWrapper = document.createElement("div");
+    this.screenshotWrapper.classList.add("screenshot-item");
+
     const screenshot = document.createElement("img");
     screenshot.src = this.screenshotUrl;
-    this.screenshotContainer.appendChild(screenshot);
+    this.screenshotWrapper.appendChild(screenshot);
 
-    const saveButton = document.createElement("button");
-    saveButton.textContent = "Save this screenshot";
-    saveButton.classList.add("save-btn");
-    this.screenshotContainer.appendChild(saveButton);
-    saveButton.addEventListener("click", () => this.save());
+    if (!this.isSaved) {
+      const saveButton = document.createElement("button");
+      saveButton.textContent = "Save this screenshot";
+      saveButton.classList.add("save-btn");
+      this.screenshotWrapper.appendChild(saveButton);
+      saveButton.addEventListener("click", () => this.save());
+    }
 
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete this screenshot";
     deleteButton.classList.add("delete-btn");
-    this.screenshotContainer.appendChild(deleteButton);
+    this.screenshotWrapper.appendChild(deleteButton);
     deleteButton.addEventListener("click", () => this.delete());
 
-    this.screenshotContainer.appendChild(this.screenshotContainer);
+    this.screenshotContainer.appendChild(this.screenshotWrapper);
   }
 
   save() {
@@ -52,7 +59,7 @@ export default class Screenshot {
   delete() {
     try {
       this.apiService.deleteScreenshot(this.screenshotUrl);
-      this.screenshotContainer.remove();
+      this.screenshotWrapper.remove();
     } catch (error) {
       console.error("Delete error:", error);
       this.onError?.(error);
