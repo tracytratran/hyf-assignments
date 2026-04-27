d7c74a9b7390b16e50e18fba3b88b952b491cfff
Tracy Trà Trần <tratran.315@gmail.com>
2026-04-28 01:04:22 +0200

    Create PlanetCard component and add styling to it

---
+++ courses/frontend/react/space-galactica-app/src/pages/DestinationPage/PlanetCard.jsx
@@ -0,0 +1,24 @@
+import styles from "./PlanetCard.module.css";
+
+export default function PlanetCard({
+  name,
+  description,
+  thumbnail,
+  isSelected,
+  togglePlanetSelection,
+}) {
+  return (
+    <div className={styles.planetCard}>
+      <img className={styles.planetThumbnail} src={thumbnail} alt="" />
+      <div className={styles.planetDescription}>
+        <h2>
+          {name} {isSelected ? "- SELECTED" : ""}
+        </h2>
+        <p>{description}</p>
+      </div>
+      <button className="roundButton" onClick={togglePlanetSelection}>
+        {isSelected ? "REMOVE FROM WISHLIST" : "ADD TO WISHLIST"}
+      </button>
+    </div>
+  );
+}
