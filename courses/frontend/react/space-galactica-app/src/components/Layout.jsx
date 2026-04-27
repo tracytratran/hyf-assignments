0d0ef8b44dbfc88c5d8f2c4ea80ef2ce4f11b1c0
Tracy Trà Trần <tratran.315@gmail.com>
2026-04-28 00:04:56 +0200

    Remove comment for task - week 1 to avoid confusion

--- courses/frontend/react/space-galactica-app/src/components/Layout.jsx
+++ courses/frontend/react/space-galactica-app/src/components/Layout.jsx
@@ -12,8 +12,6 @@ export const RootLayout = () => {
     <div className="inter">
       <Navbar />
       <Outlet />
-      {/* 🧑🏽‍🚀 Task - Week 1 */}
-      {/* Import and use the Footer component here. */}
       <Footer />
     </div>
   );
