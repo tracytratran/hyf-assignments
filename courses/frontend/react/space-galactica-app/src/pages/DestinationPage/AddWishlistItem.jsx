5679608e09a04cf21c82bc4c770bf66c210d6efe
Tracy Trà Trần <tratran.315@gmail.com>
2026-05-04 22:01:08 +0200

    Handle the input with the id of customWishList

--- courses/frontend/react/space-galactica-app/src/pages/DestinationPage/AddWishlistItem.jsx
+++ courses/frontend/react/space-galactica-app/src/pages/DestinationPage/AddWishlistItem.jsx
@@ -1,28 +1,32 @@
-import { useState } from 'react';
-import styles from './DestinationPage.module.css';
+import { use, useState } from "react";
+import styles from "./DestinationPage.module.css";
 
-export const AddWishlistItem = ({
-  onAddWishlistItem,
-}) => {
-  const [thumbnail, setThumbnail] = useState('/destination/image-europa.png');
+export const AddWishlistItem = ({ onAddWishlistItem }) => {
+  const [thumbnail, setThumbnail] = useState("/destination/image-europa.png");
   // 🧑🏽‍🚀 Task - Week 3
   // Add a useState for the handling of the <input id="customWishlist" type="text" />.
   // Connect the setThumbnail to the <select>.
+  const [customWishListItem, setCustomWishListItem] = useState("");
 
   const onAddItemPressed = () => {
     // 🧑🏽‍🚀 Task - Week 3
     // The required functionaity here:
     // - call the onAddWishlistItem function;
     // - clear the <input/> field .
-  }
+  };
 
   return (
     <div className={styles.addWishlistItem}>
       <p>Add custom planet to wishlist</p>
       <label htmlFor="customWishlist">Wishlist item name</label>
-      <input id="customWishlist" type="text" />
+      <input
+        id="customWishlist"
+        type="text"
+        value={customWishListItem}
+        onChange={(e) => setCustomWishListItem(e.currentTarget.value)}
+      />
       <label htmlFor="customWishlistThumbnail">Wishlist item thumbnail</label>
-      <select id="customWishlistThumbnail" >
+      <select id="customWishlistThumbnail">
         <option value="/destination/image-europa.png">EUROPA</option>
         <option value="/destination/image-mars.png">MARS</option>
         <option value="/destination/image-moon.png">MOON</option>
