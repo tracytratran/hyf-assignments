850b5186eab973df89c028e9080c0fb08a9ca1da
Tracy Trà Trần <tratran.315@gmail.com>
2026-05-13 00:10:33 +0200

    fix: Remove API Key and handle fetch error with try/catch in NasaCollaborationPage

--- courses/frontend/react/space-galactica-app/src/pages/NasaCollaborationPage/NasaCollaborationPage.jsx
+++ courses/frontend/react/space-galactica-app/src/pages/NasaCollaborationPage/NasaCollaborationPage.jsx
@@ -1,16 +1,11 @@
-import React, { useState, useEffect } from "react";
-import styles from "./NasaCollaborationPage.module.css";
+import { useEffect, useState } from "react";
+import { NASA_API_KEY } from "../../../config.js";
 import RoverPhoto from "../../components/RoverPhoto";
-
-// Read "/app/nasa_collaboration/README.md" for more info about the API_KEY
-const API_KEY = "6V87kaVneIgHP8vr6xVebecrF2qGVQNo7SVck32F";
+import styles from "./NasaCollaborationPage.module.css";
 
 const NASA_URLs = {
-  astronomyPicOfTheDay: `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`,
+  astronomyPicOfTheDay: `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`,
 
-  // marsRoverPhoto: `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=${API_KEY}`,
-
-  // Alternative API as the Mars Rover API has been archived
   marsRoverPhoto:
     "https://rovers.nebulum.one/api/v1/rovers/curiosity/photos?earth_date=2015-6-3",
 };
@@ -18,30 +13,45 @@ const NASA_URLs = {
 export const NasaCollaboration = () => {
   const [dailyImg, setDailyImg] = useState({});
   const [roverPhoto, setRoverPhoto] = useState({});
+  const [error, setError] = useState(null);
 
-  // 🧑🏽‍🚀 Task - Week 3
-  // Fetch the extra data for NASA_URLs.astronomyPicOfTheDay and save it to the dailyImg state variable.
   useEffect(() => {
     const fetchAstronomyPicOfTheDay = async () => {
-      const astronomyPicOfTheDayResponse = await fetch(
-        NASA_URLs.astronomyPicOfTheDay,
-      ).then((response) => response.json());
-      setDailyImg(astronomyPicOfTheDayResponse);
-    };
+      try {
+        setError(null);
 
+        const response = await fetch(NASA_URLs.astronomyPicOfTheDay);
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+
+        const astronomyPicOfTheDayData = await response.json();
+        setDailyImg(astronomyPicOfTheDayData);
+      } catch (error) {
+        setError("Failed to load the astronomy picture. Please try again!");
+      }
+    };
     fetchAstronomyPicOfTheDay();
   }, []);
 
   useEffect(() => {
     const fetchRoverPhotos = async () => {
-      const roverPhotoResponse = await fetch(NASA_URLs.marsRoverPhoto).then(
-        (response) => response.json(),
-      );
-      setRoverPhoto(roverPhotoResponse);
-    };
+      try {
+        const response = await fetch(NASA_URLs.marsRoverPhoto);
+        if (!response.ok) {
+          throw new Error(`HTTP Error: ${response.status}`);
+        }
 
+        const roverPhotoData = await response.json();
+        setRoverPhoto(roverPhotoData);
+      } catch (error) {
+        setError("Failed to load the rover photo. Please try again!");
+      }
+    };
     fetchRoverPhotos();
   }, []);
+
+  if (error) return <p>Error: {error}</p>;
 
   return (
     <div className="fullBGpicture">
@@ -49,9 +59,6 @@ export const NasaCollaboration = () => {
         <h1>Collaboration with NASA</h1>
         <section className="card">
           <h2>Astronomy Picture of the day</h2>
-          {/* 🧑🏽‍🚀 Task - Week 3 */}
-          {/* After fetching data from the NASA_URLs.astronomyPicOfTheDay url, display the returned data here. */}
-          {/* You should display the title, explanation, and the image using the url from the response */}
           {dailyImg ? (
             <div className={styles.nasaPicOfTheDay}>
               <h3>{dailyImg.title}</h3>
@@ -64,10 +71,8 @@ export const NasaCollaboration = () => {
         </section>
         <section className="card">
           <h2>Rover Photos</h2>
-          {/* 🧑🏽‍🚀 Task - Week 3 */}
-          {/* Iterate over the roverPhoto?.photos array and display all the pictures. */}
           {roverPhoto?.photos?.length ? (
-            <div className={styles.photosWrapper}>
+            <div className={styles.roverPhotosWrapper}>
               {roverPhoto.photos.map((photo) => (
                 <RoverPhoto
                   key={photo.id}
